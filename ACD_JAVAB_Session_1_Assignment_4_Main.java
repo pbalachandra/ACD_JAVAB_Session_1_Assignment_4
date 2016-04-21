a73ab366c6876041e155dae850d9afa3baf03414
pbalachandra <balachandra.p@gmail.com>
2016-04-21 14:05:49 +0530

    ACD_JAVAB_Session_1_Assignment_4_Main
    ACD_JAVAB_Session_1_Assignment_4_Main

---
+++ ACD_JAVAB_Session_1_Assignment_4_Main.java
@@ -0,0 +1,33 @@
+/**
+ * @author BALU
+ *
+ */
+package session1;
+
+public class ACD_JAVAB_Session_1_Assignment_4_Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x = 5, y = 10;
+		int a, b, c, d;
+		a = x + y * 2;
+		b = x - y + 2;
+		c = (x + y) * 2;
+		d = y % x;
+		System.out.println("a : " + a);
+		System.out.println("b : " + b);
+		System.out.println("c : " + c);
+		System.out.println("d : " + d);
+	}
+
+}
+/* OUTPUT
+ * a : 25
+ * b : -3
+ * c : 30
+ * d : 0
+ */
+  
